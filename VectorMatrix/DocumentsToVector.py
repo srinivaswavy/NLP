05cb1b6775a72f5bf7f182d23c2933672acfffdb
srinivaswavy <srinivaswavy@gmail.com>
2023-12-17 22:57:50 +0530

    Refactored

--- VectorMatrix/DocumentsToVector.py
+++
@@ -1,8 +0,0 @@
-# from scipy import *
-from numpy import *
-
-data = array(["Hi! How are you?", "I am Srini", "After that it's all blah blah blah"], )
-
-print(data)
-
-
---
+++ VectorMatrix/src/DocumentsToVector.py
@@ -0,0 +1,10 @@
+from numpy import *
+
+data = array(["Hi! How are you?", "I am Srini", "After that it's all blah blah blah"])
+data2 = array([1,2,3])
+
+data2 = data.copy()
+
+print(data)
+
+
