6c9a32840daf9c371e9a2b83b36b73a50ac0e373
srinivaswavy <srinivaswavy@gmail.com>
2023-12-18 20:59:20 +0530

    Added with statement

--- VectorMatrix/src/CountVectoization.py
+++ VectorMatrix/src/CountVectoization.py
@@ -1,8 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 vectorizer = CountVectorizer()
-datafile = open('data','r')
-X = vectorizer.fit_transform(datafile)
+with open('data','r') as datafile:
+    X = vectorizer.fit_transform(datafile)
 vocabolary = vectorizer.get_feature_names_out()
 print(X.toarray())
 print(vocabolary)
