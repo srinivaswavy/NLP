c979221858fba01a2cf47c6d89f7a177fd9af365
srinivaswavy <srinivaswavy@gmail.com>
2023-12-18 13:02:12 +0530

    Count vectorization

--- VectorMatrix/src/DocumentsToVector.py
+++ VectorMatrix/src/DocumentsToVector.py
@@ -1,10 +1,36 @@
 from numpy import *
 
 data = array(["Hi! How are you?", "I am Srini", "After that it's all blah blah blah"])
-data2 = array([1,2,3])
 
-data2 = data.copy()
+flattenedWordData = list(map(lambda item: item.split(" "), data))
 
-print(data)
+uniqueWords = unique([item for row in flattenedWordData for item in row])
+
+vocabulary = {}
+i=0
+for item in uniqueWords:
+    vocabulary[item] = i
+    i+=1
+
+print(vocabulary)
+
+def transformTextToVector( str:str):
+    vector = zeros(len(vocabulary),int)
+    for word in str.split(" "):
+        index = vocabulary[word]
+        vector[index] = vector[index]+1
+    return vector
+
+vectors = []
+
+for sentence in data:
+    vectors.append(transformTextToVector(sentence))
+
+print(array(vectors))
+
+
+
+
+
 
 
