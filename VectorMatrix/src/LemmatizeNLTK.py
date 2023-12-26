22b9016208a414456422cfa368cba4159bb654cb
srinivaswavy <srinivaswavy@gmail.com>
2023-12-26 16:55:46 +0530

    Lemmatizer NLTK

---
+++ VectorMatrix/src/LemmatizeNLTK.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+nltk.download("wordnet")
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("mice"))
+print(lemmatizer.lemmatize("replacement",pos=wordnet.ADJ))
+print(lemmatizer.lemmatize("is",pos=wordnet.NOUN))
+print(lemmatizer.lemmatize("blowing",pos=wordnet.VERB))
+print(lemmatizer.lemmatize("going"))
