7f5aeb88b3049211e4dd184473c44113f21df79f
srinivaswavy <srinivaswavy@gmail.com>
2024-04-17 14:45:10 +0530

    Update TopicModelingLDA.py

--- VectorMatrix/src/TopicModelingLDA.py
+++ VectorMatrix/src/TopicModelingLDA.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation as lda
 from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+import numpy as np
+import textwrap
 
 nltk.download('stopwords')
 df = pd.read_csv('bbc_text_cls.csv')
@@ -14,10 +17,59 @@ count_vectorizer_model = count_vectorizer.fit(df['text'])
 X = count_vectorizer_model.transform(df['text'])
 print(X)
 
+
+def plot_top_words(model, feature_names, n_top_words=10):
+    fig, axes = plt.subplots(2, 5, figsize=(30, 15), sharex=True)
+    axes = axes.flatten()
+    for topic_idx, topic in enumerate(model.components_):
+        top_features_ind = topic.argsort()[: -n_top_words - 1: -1]
+        top_features = [feature_names[i] for i in top_features_ind]
+        weights = topic[top_features_ind]
+
+        ax = axes[topic_idx]
+        ax.barh(top_features, weights, height=0.7)
+        ax.set_title(f"Topic {topic_idx + 1}", fontdict={"fontsize": 30})
+        ax.invert_yaxis()
+        ax.tick_params(axis="both", which="major", labelsize=20)
+        for i in "top right left".split():
+            ax.spines[i].set_visible(False)
+        fig.suptitle('LDA', fontsize=40)
+
+    plt.subplots_adjust(top=0.90, bottom=0.05, wspace=0.90, hspace=0.3)
+    plt.show()
+
+
 lda_model = lda(n_components=10, random_state=0)
 lda_model.fit(X)
 
-print(lda_model.components_)
+feature_names = count_vectorizer.get_feature_names_out()
+plot_top_words(lda_model, feature_names);
 
-lda_model.transform(X)
+Z = lda_model.transform(X)
 
+np.random.seed(0)
+i = np.random.choice(len(df))
+z = Z[i]
+topics = np.arange(10) + 1
+
+fig, ax = plt.subplots()
+ax.barh(topics, z)
+ax.set_yticks(topics)
+ax.set_title('True label: %s' % df.iloc[i]['labels']);
+
+
+def wrap(x):
+  return textwrap.fill(x, replace_whitespace=False, fix_sentence_endings=True)
+
+print(wrap(df.iloc[i]['text']))
+
+
+i = np.random.choice(len(df))
+z = Z[i]
+
+fig, ax = plt.subplots()
+ax.barh(topics, z)
+ax.set_yticks(topics)
+ax.set_title('True label: %s' % df.iloc[i]['labels'])
+
+plt.show()
