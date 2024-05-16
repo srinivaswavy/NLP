4bed7e7f77c1dc75e6ef1d6066f6f459f0fd3945
srinivaswavy <srinivaswavy@gmail.com>
2024-05-16 21:45:50 +0530

    Pos

---
+++ VectorMatrix/src/POSTaggingRNN.py
@@ -0,0 +1,264 @@
+import nltk
+from nltk.corpus import brown
+
+nltk.download('brown')
+nltk.download('universal_tagset')
+
+corpus = brown.tagged_sents(tagset='universal')
+
+print(corpus[0:10])
+
+print(len(corpus))
+
+inputs = []
+targets = []
+
+for sentence_tag_pairs in corpus:
+    tokens = []
+    target = []
+    for token, tag in sentence_tag_pairs:
+        tokens.append(token)
+        target.append(tag)
+    inputs.append(tokens)
+    targets.append(target)
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Input, Bidirectional
+from keras.layers import LSTM, GRU, SimpleRNN, Embedding
+from keras.models import Model
+from keras.losses import SparseCategoricalCrossentropy
+
+train_inputs, test_inputs, train_targets, test_targets = train_test_split(
+    inputs,
+    targets,
+    test_size=0.3,
+)
+
+# Convert sentences to sequences
+
+MAX_VOCAB_SIZE = None
+
+# capitalization might be useful - test it
+should_lowercase = False
+word_tokenizer = Tokenizer(
+    num_words=MAX_VOCAB_SIZE,
+    lower=should_lowercase,
+    oov_token='UNK',
+)
+# otherwise unknown tokens will be removed and len(input) != len(target)
+# input words and target words will not be aligned!
+
+word_tokenizer.fit_on_texts(train_inputs)
+train_inputs_int = word_tokenizer.texts_to_sequences(train_inputs)
+test_inputs_int = word_tokenizer.texts_to_sequences(test_inputs)
+
+# get word -> integer mapping
+word2idx = word_tokenizer.word_index
+V = len(word2idx)
+print('Found %s unique tokens.' % V)
+
+
+def flatten(list_of_lists):
+    flattened = [val for sublist in list_of_lists for val in sublist]
+    return flattened
+
+
+all_train_targets = set(flatten(train_targets))
+print(all_train_targets)
+
+all_test_targets = set(flatten(test_targets))
+print(all_test_targets)
+
+print(all_train_targets == all_test_targets)
+
+# Convert targets to sequences
+tag_tokenizer = Tokenizer()
+tag_tokenizer.fit_on_texts(train_targets)
+train_targets_int = tag_tokenizer.texts_to_sequences(train_targets)
+test_targets_int = tag_tokenizer.texts_to_sequences(test_targets)
+
+# save for later
+train_targets_int_unpadded = train_targets_int
+test_targets_int_unpadded = test_targets_int
+
+# before padding, find max document length
+# because we don't want to truncate any inputs
+# which would also truncate targets
+maxlen_train = max(len(sent) for sent in train_inputs)
+maxlen_test = max(len(sent) for sent in test_inputs)
+T = max((maxlen_train, maxlen_test))
+
+
+# pad sequences so that we get a N x T matrix
+train_inputs_int = pad_sequences(train_inputs_int, maxlen=T)
+print('Shape of data train tensor:', train_inputs_int.shape)
+
+test_inputs_int = pad_sequences(test_inputs_int, maxlen=T)
+print('Shape of data test tensor:', test_inputs_int.shape)
+
+train_targets_int = pad_sequences(train_targets_int, maxlen=T)
+print('Shape of train targets tensor:', train_targets_int.shape)
+
+test_targets_int = pad_sequences(test_targets_int, maxlen=T)
+print('Shape of test targets tensor:', test_targets_int.shape)
+
+K = len(tag_tokenizer.word_index) + 1
+print(K)
+
+# Create the model
+
+# We get to choose embedding dimensionality
+D = 32
+
+i = Input(shape=(T,))
+# mask_zero=True way slower on GPU than CPU!
+x = Embedding(V + 1, D, mask_zero=True)(i)
+x = Bidirectional(LSTM(32, return_sequences=True))(x)
+x = Dense(K)(x)
+
+model = Model(i, x)
+
+# Compile and fit
+model.compile(
+  loss=SparseCategoricalCrossentropy(from_logits=True),
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+# NOTE: you "could" speed this up by creating a custom loss, since Tensorflow's
+# implementation of mask_zero currently sucks, but it's sufficiently advanced
+# to be outside the scope of this course
+# In my experiments, CPU is faster than GPU in all cases, and CPU for custom
+# loss is faster than CPU for mask_zero
+
+# > 300-400s per epoch on CPU
+# > 30 MINUTES per epoch on GPU
+print('Training model...')
+r = model.fit(
+  train_inputs_int,
+  train_targets_int,
+  epochs=5,
+  validation_data=(test_inputs_int, test_targets_int)
+)
+
+# Plot loss per iteration
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+
+plt.show()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='train acc')
+plt.plot(r.history['val_accuracy'], label='val acc')
+plt.legend()
+
+plt.show()
+
+
+# True model accuracy - above includes padding
+
+# first get length of each sequence
+train_lengths = []
+for sentence in train_inputs:
+  train_lengths.append(len(sentence))
+
+test_lengths = []
+for sentence in test_inputs:
+  test_lengths.append(len(sentence))
+
+train_probs = model.predict(train_inputs_int) # N x T x K
+train_predictions = []
+for probs, length in zip(train_probs, train_lengths):
+  # probs is T x K
+  probs_ = probs[-length:]
+  preds = np.argmax(probs_, axis=1)
+  train_predictions.append(preds)
+
+# flatten
+flat_train_predictions = flatten(train_predictions)
+flat_train_targets = flatten(train_targets_int_unpadded)
+
+test_probs = model.predict(test_inputs_int) # N x T x K
+test_predictions = []
+for probs, length in zip(test_probs, test_lengths):
+  # probs is T x K
+  probs_ = probs[-length:]
+  preds = np.argmax(probs_, axis=1)
+  test_predictions.append(preds)
+
+# flatten
+flat_test_predictions = flatten(test_predictions)
+flat_test_targets = flatten(test_targets_int_unpadded)
+
+from sklearn.metrics import accuracy_score, f1_score
+
+print("Train acc:", accuracy_score(flat_train_targets, flat_train_predictions))
+print("Test acc:", accuracy_score(flat_test_targets, flat_test_predictions))
+
+print("Train f1:",
+      f1_score(flat_train_targets, flat_train_predictions, average='macro'))
+print("Test f1:",
+      f1_score(flat_test_targets, flat_test_predictions, average='macro'))
+
+
+### Baseline model: map word to tag
+from collections import Counter
+
+# https://stackoverflow.com/questions/1518522/find-the-most-common-element-in-a-list
+def most_common(lst):
+  data = Counter(lst)
+  return data.most_common(1)[0][0]
+
+token2tags = {k: [] for k, v in word2idx.items()}
+
+# remove UNK token
+del token2tags['UNK']
+
+for tokens, tags in zip(train_inputs, train_targets):
+  for token, tag in zip(tokens, tags):
+    if should_lowercase:
+      token = token.lower()
+    if token in token2tags:
+      token2tags[token].append(tag)
+
+token2tag = {k: most_common(v) for k, v in token2tags.items()}
+
+# compute accuracy
+
+train_predictions = []
+for sentence in train_inputs:
+  predictions = []
+  for token in sentence:
+    if should_lowercase:
+      token = token.lower()
+    predicted_tag = token2tag[token]
+    predictions.append(predicted_tag)
+  train_predictions.append(predictions)
+flat_train_predictions = flatten(train_predictions)
+flat_train_targets = flatten(train_targets)
+
+test_predictions = []
+for sentence in test_inputs:
+  predictions = []
+  for token in sentence:
+    predicted_tag = token2tag.get(token, 'INCORRECT')
+    predictions.append(predicted_tag)
+  test_predictions.append(predictions)
+flat_test_predictions = flatten(test_predictions)
+flat_test_targets = flatten(test_targets)
+
+print("Train acc:", accuracy_score(flat_train_targets, flat_train_predictions))
+print("Test acc:", accuracy_score(flat_test_targets, flat_test_predictions))
+
+print("Train f1:",
+      f1_score(flat_train_targets, flat_train_predictions, average='macro'))
+print("Test f1:",
+      f1_score(flat_test_targets, flat_test_predictions, average='macro'))
--- VectorMatrix/src/Sample.py
+++ VectorMatrix/src/Sample.py
@@ -1,16 +1,18 @@
-import numpy as np
+# import numpy as np
+#
+# # Let's assume the following is your numpy array
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#
+# # You can divide each element of a row by the sum of elements in that row as follows:
+# result = a / a.sum(axis=1,keepdims=True)
+#
+# print(result)
+#
+# print(a.sum(axis=1,keepdims=True))
+# print(a.sum(axis=1,keepdims=False))
+#
+# result = a / a.sum(axis=1,keepdims=False)
+#
+# print(result)
 
-# Let's assume the following is your numpy array
-a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-# You can divide each element of a row by the sum of elements in that row as follows:
-result = a / a.sum(axis=1,keepdims=True)
-
-print(result)
-
-print(a.sum(axis=1,keepdims=True))
-print(a.sum(axis=1,keepdims=False))
-
-result = a / a.sum(axis=1,keepdims=False)
-
-print(result)
